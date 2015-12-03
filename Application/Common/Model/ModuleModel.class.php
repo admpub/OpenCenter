593efe4faf6a2bb362892e54a4adc924b15912a2
Wenhui Shen <swhbox@foxmail.com>
2015-12-03 14:28:24 +0800

    fixed bug

--- Application/Common/Model/ModuleModel.class.php
+++ Application/Common/Model/ModuleModel.class.php
@@ -9,156 +9,144 @@
 
 namespace Common\Model;
 
-class ModuleModel extends Base
-{
+class ModuleModel extends Base {
 
-    protected $tableName = 'module';
+	protected $tableName = 'module';
 
-    public function getAll()
-    {
+	public function getAll() {
 
-        $module = S('module_all');
-        if (empty($module)) {
-            $dir = $this->getFile(APP_PATH);
-            foreach ($dir as $subdir) {
-                if (file_exists(APP_PATH . '/' . $subdir . '/Info/info.php')) {
-                    $info = $this->getInfo($subdir);
-                    $info = $this->getModule($subdir);
-                    $info['can_uninstall'] = file_exists(APP_PATH . '/' . $info['name'] . '/Info/uninstall.sql');
-                    $module[] = $info;
-                }
-            }
+		$module = S('module_all');
+		if (empty($module)) {
+			$dir = $this->getFile(APP_PATH);
+			foreach ($dir as $subdir) {
+				if (file_exists(APP_PATH . '/' . $subdir . '/Info/info.php')) {
+					$info = $this->getInfo($subdir);
+					$info = $this->getModule($subdir);
+					$info['can_uninstall'] = file_exists(APP_PATH . '/' . $info['name'] . '/Info/uninstall.sql');
+					$module[] = $info;
+				}
+			}
 
-            S('module_all', $module);
-        }
+			S('module_all', $module);
+		}
 
-        return $module;
-    }
+		return $module;
+	}
 
-    public function checkCanVisit($name)
-    {
-        $modules = $this->getAll();
+	public function checkCanVisit($name) {
+		$modules = $this->getAll();
 
-        foreach ($modules as $m) {
-            if (isset($m['is_setup']) && $m['is_setup'] == 0 && $m['name'] == ucfirst($name)) {
-                header("Content-Type: text/html; charset=utf-8");
-                exit('您所访问的模块未安装，禁止访问。');
-            }
-        }
+		foreach ($modules as $m) {
+			if (isset($m['is_setup']) && $m['is_setup'] == 0 && $m['name'] == ucfirst($name)) {
+				header('Content-Type: text/html; charset=utf-8');
+				exit('您所访问的模块未安装，禁止访问。');
+			}
+		}
 
-    }
+	}
 
-    private function  cleanModulesCache()
-    {
-        S('module_all', null);
-    }
+	private function cleanModulesCache() {
+		S('module_all', null);
+	}
 
-    public function uninstall($id)
-    {
-        $module = $this->find($id);
-        if ($module['is_setup'] == 0) {
-            return array('error_code' => '模块未安装。');
-        }
-        $uninstallSql = APP_PATH . '/' . $module['name'] . '/Info/uninstall.sql';
-        $res = D()->executeSqlFile($uninstallSql);
+	public function uninstall($id) {
+		$module = $this->find($id);
+		if ($module['is_setup'] == 0) {
+			return array('error_code' => '模块未安装。');
+		}
+		$uninstallSql = APP_PATH . '/' . $module['name'] . '/Info/uninstall.sql';
+		$res = $this->executeSqlFile($uninstallSql);
 
-        if ($res === true) {
-            $module['is_setup'] = 0;
-            $this->save($module);
-        }
-        $this->cleanModulesCache();
-        return $res;
-    }
+		if ($res === true) {
+			$module['is_setup'] = 0;
+			$this->save($module);
+		}
+		$this->cleanModulesCache();
+		return $res;
+	}
 
-    public function install($id)
-    {
-        $module = $this->find($id);
-        if ($module['is_setup'] == 1) {
-            return array('error_code' => '模块已安装。');
-        }
-        $uninstallSql = APP_PATH . '/' . $module['name'] . '/Info/install.sql';
-        $res = D()->executeSqlFile($uninstallSql);
+	public function install($id) {
+		$module = $this->find($id);
+		if ($module['is_setup'] == 1) {
+			return array('error_code' => '模块已安装。');
+		}
+		$uninstallSql = APP_PATH . '/' . $module['name'] . '/Info/install.sql';
+		$res = $this->executeSqlFile($uninstallSql);
 
-        if ($res === true) {
-            $module['is_setup'] = 1;
-            $this->save($module);
-        }
-        clean_all_cache();//清除全站缓存
-        return $res;
-    }
+		if ($res === true) {
+			$module['is_setup'] = 1;
+			$this->save($module);
+		}
+		clean_all_cache(); //清除全站缓存
+		return $res;
+	}
 
-    /**检查模块是否已安装
-     * @param $name
-     * @auth 陈一枭
-     */
-    public function getModule($name)
-    {
-        $module = $this->where(array('name' => $name))->find();
-        if (!$module) {
-            $m = $this->getInfo($name);
-            if ($m['can_uninstall']) {
-                $m['is_setup'] = 0;//默认设为已安装，防止已安装的模块反复安装。
-            } else {
-                $m['is_setup'] = 1;
-            }
-            $m['id'] = $this->add($m);
-            return $m;
-        } else {
-            return $module;
-        }
-    }
+	/**检查模块是否已安装
+		     * @param $name
+		     * @auth 陈一枭
+	*/
+	public function getModule($name) {
+		$module = $this->where(array('name' => $name))->find();
+		if (!$module) {
+			$m = $this->getInfo($name);
+			if ($m['can_uninstall']) {
+				$m['is_setup'] = 0; //默认设为已安装，防止已安装的模块反复安装。
+			} else {
+				$m['is_setup'] = 1;
+			}
+			$m['id'] = $this->add($m);
+			return $m;
+		} else {
+			return $module;
+		}
+	}
 
-    private function getInfo($name)
-    {
-        if (file_exists(APP_PATH . '/' . $name . '/Info/info.php')) {
-            $module = require(APP_PATH . '/' . $name . '/Info/info.php');
-            return $module;
-        } else {
-            return array();
-        }
+	private function getInfo($name) {
+		if (file_exists(APP_PATH . '/' . $name . '/Info/info.php')) {
+			$module = require APP_PATH . '/' . $name . '/Info/info.php';
+			return $module;
+		} else {
+			return array();
+		}
 
-    }
+	}
 
-    /**
-     * 获取文件列表
-     */
-    private function getFile($folder)
-    {
-        //打开目录
-        $fp = opendir($folder);
-        //阅读目录
-        while (false != $file = readdir($fp)) {
-            //列出所有文件并去掉'.'和'..'
-            if ($file != '.' && $file != '..') {
-                //$file="$folder/$file";
-                $file = "$file";
+	/**
+	 * 获取文件列表
+	 */
+	private function getFile($folder) {
+		//打开目录
+		$fp = opendir($folder);
+		//阅读目录
+		while (false != $file = readdir($fp)) {
+			//列出所有文件并去掉'.'和'..'
+			if ($file != '.' && $file != '..') {
+				//$file="$folder/$file";
+				$file = "$file";
 
-                //赋值给数组
-                $arr_file[] = $file;
+				//赋值给数组
+				$arr_file[] = $file;
 
-            }
-        }
-        //输出结果
-        if (is_array($arr_file)) {
-            while (list($key, $value) = each($arr_file)) {
-                $files[] = $value;
-            }
-        }
-        //关闭目录
-        closedir($fp);
-        return $files;
+			}
+		}
+		//输出结果
+		if (is_array($arr_file)) {
+			while (list($key, $value) = each($arr_file)) {
+				$files[] = $value;
+			}
+		}
+		//关闭目录
+		closedir($fp);
+		return $files;
 
+	}
 
-    }
-
-
-    public function isInstalled($name)
-    {
-        $module = $this->getModule($name);
-        if ($module['is_setup']) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-} 
+	public function isInstalled($name) {
+		$module = $this->getModule($name);
+		if ($module['is_setup']) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
