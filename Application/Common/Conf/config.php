593563c1736b94fd26beabf7e5f155a746ce8e3d
swh <swhbox@foxmail.com>
2015-12-10 13:41:49 +0800

    update

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -8,4 +8,14 @@ if (is_file('./Conf/common.php')) {
 $config['TMPL_ACTION_ERROR'] = COMMON_PATH . 'View/default/Public/error.html'; // 默认错误跳转对应的模板文件
 $config['TMPL_ACTION_SUCCESS'] = COMMON_PATH . 'View/default/Public/success.html'; // 默认成功跳转对应的模板文件
 $config['TMPL_EXCEPTION_FILE'] = COMMON_PATH . 'View/default/Public/exception.html'; // 异常页面的模板文件
+
+if (isset($config['DATA_CACHE_TYPE'])) {
+	return $config;
+}
+
+$config['DATA_CACHE_TYPE'] = 'File'; // 数据缓存类型,支持:File|Db|Apc|Memcache|Shmop|Sqlite|Xcache|Apachenote|Eaccelerator
+$config['MEMCACHE_HOST'] = '127.0.0.1';
+$config['MEMCACHE_PORT'] = 11211;
+$config['DATA_CACHE_TIMEOUT'] = 86400;
+
 return $config;
