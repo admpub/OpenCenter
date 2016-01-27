7065ab5fd0cfefe5f0c86446ed21efc262bd595f
swh <swhbox@foxmail.com>
2016-01-27 12:17:15 +0800

    update

--- Application/Common/Common/extend.php
+++ Application/Common/Common/extend.php
@@ -110,3 +110,27 @@ function homeUrl() {
 	}
 	return U('Home/Index/index');
 }
+
+/**
+ * 检查远程文件是否存在
+ * @param  string $url 远程文章完整网址
+ * @return bool
+ */
+function check_remote_file_exists($url) {
+	$curl = curl_init($url);
+	// 不取回数据
+	curl_setopt($curl, CURLOPT_NOBODY, true);
+	// 发送请求
+	$result = curl_exec($curl);
+	$found = false;
+	// 如果请求没有发送失败
+	if ($result !== false) {
+		// 再检查http响应码是否为200
+		$statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		if ($statusCode == 200) {
+			$found = true;
+		}
+	}
+	curl_close($curl);
+	return $found;
+}
